09ba0e6add877a46da50724f15d12981bb44e11b
rokushodev <jmainfocell@gmail.com>
2022-03-09 22:45:15 -0300

    version - 0.2

--- main.py
+++ main.py
@@ -10,8 +10,35 @@ import time
 
 
 
-cnx = mysql.connector.connect(user='root', database='bot49')
-print('Conectado com sucesso ao banco de dados')
+# cnx = mysql.connector.connect(user='root', database='bot')
+# print('Conectado com sucesso ao banco de dados')
+
+
+#  testando conexão
+def check_connction(database, user):
+    conn = None
+    try:
+        conn = mysql.connector.connect()
+        print('Conectado com sucesso!')
+    except:
+        print(f'erro ao conectar: {"error"}')
+    return conn
+
+
+conn = check_connction(database='bot', user='root')
+
+
+#  query consultas
+# def exec_query(conn, query):
+#     cursor = conn.cursor()
+#     try:
+#         cursor.execute(query)
+#         conn.commit()
+#         print('sucesso na query')
+#     except:
+#         print('error')
+#
+
 
 #  salvando sessão
 dir_path = os.getcwd()
@@ -26,6 +53,12 @@ driver = webdriver.Chrome("./chromedriver.exe", chrome_options=options)
 
 driver.get('https://web.whatsapp.com/')
 time.sleep(8)
+
+
+
+
+
+
 
 while True:
 
@@ -42,8 +75,11 @@ while True:
 
         #  pegando nome ou numero de telefone
         elem = driver.find_element(By.CLASS_NAME, '_21nHd')
-        telefone = elem.text
-        print(telefone)
+
+        contato = elem.text
+        print(contato)
+
+
 
         #  pegando ultima msg
         elem = driver.find_elements(By.CLASS_NAME, '_1Gy50')
@@ -51,20 +87,28 @@ while True:
         todas_msg_texto = [e.text for e in elem]
 
         #  respondendo a mensagem
-        elem = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
-        elem.send_keys('Olá boa noite')
 
+
+        elem = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
+        elem.send_keys('Digite uma opçao:\n'
+                       '1 - Boleto\n'
+                       '2 - Falar com tecnico\n'
+                       '3 - Encerrar')
 
         elem.send_keys(Keys.RETURN)
-
-
 
         #  pegando ultima msg
         ultima_msg = todas_msg_texto[-1]
         print(ultima_msg)
 
+        if ultima_msg == '1':
+
+            print('abrir boleto')
 
 
+
+        else:
+            pass
 
 
     except:
